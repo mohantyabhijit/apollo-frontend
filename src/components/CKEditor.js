e9790a2d8b0c046cf760e24d60bafcd61b8c3cae
Abhijit Mohanty <8387545+mohantyabhijit@users.noreply.github.com>
2021-10-08 22:22:02 +0530

    fetched data from ckeditor

--- src/components/CKEditor.js
+++ src/components/CKEditor.js
@@ -1,32 +1,99 @@
-import React, { Component } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// Note: We need to build the editor from source.
+// We cannot use existing builds in this case.
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
-export default function CKEditor() {
-    
-        return (
-            <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
-    
+import PendingActions from '@ckeditor/ckeditor5-core/src/pendingactions';
+
+let isDirty = false;
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [
+            PendingActions,
+
+            // ... other plugins
+        ]
+    } )
+    .then( editor => {
+        window.editor = editor;
+
+        handleStatusChanges( editor );
+        handleSaveButton( editor );
+        handleBeforeunload( editor );
+    } )
+    .catch( err => {
+        console.error( err.stack );
+    } );
+
+// Handle clicking the "Save" button by sending the data to a
+// fake HTTP server (emulated here with setTimeout()).
+function handleSaveButton( editor ) {
+    const saveButton = document.querySelector( '#save' );
+    const pendingActions = editor.plugins.get( 'PendingActions' );
+
+    saveButton.addEventListener( 'click', evt => {
+        const data = editor.getData();
+
+        // Register the action of saving the data as a "pending action".
+        // All asynchronous actions related to the editor are tracked like this,
+        // so later on you only need to check `pendingActions.hasAny` to check
+        // whether the editor is busy or not.
+        const action = pendingActions.add( 'Saving changes' );
+
+        evt.preventDefault();
+
+        // Save the data to a fake HTTP server.
+        setTimeout( () => {
+            pendingActions.remove( action );
+
+            // Reset isDirty only if the data did not change in the meantime.
+            if ( data == editor.getData() ) {
+                isDirty = false;
+            }
+
+            updateStatus( editor );
+        }, HTTP_SERVER_LAG );
+    } );
 }
 
+// Listen to new changes (to enable the "Save" button) and to
+// pending actions (to show the spinner animation when the editor is busy).
+function handleStatusChanges( editor ) {
+    editor.plugins.get( 'PendingActions' ).on( 'change:hasAny', () => updateStatus( editor ) );
+
+    editor.model.document.on( 'change:data', () => {
+        isDirty = true;
+
+        updateStatus( editor );
+    } );
+}
+
+// If the user tries to leave the page before the data is saved, ask
+// them whether they are sure they want to proceed.
+function handleBeforeunload( editor ) {
+    const pendingActions = editor.plugins.get( 'PendingActions' );
+
+    window.addEventListener( 'beforeunload', evt => {
+        if ( pendingActions.hasAny ) {
+            evt.preventDefault();
+        }
+    } );
+}
+
+function updateStatus( editor ) {
+    const saveButton = document.querySelector( '#save' );
+
+    // Disables the "Save" button when the data on the server is up to date.
+    if ( isDirty ) {
+        saveButton.classList.add( 'active' );
+    } else {
+        saveButton.classList.remove( 'active' );
+    }
+
+    // Shows the spinner animation.
+    if ( editor.plugins.get( 'PendingActions' ).hasAny ) {
+        saveButton.classList.add( 'saving' );
+    } else {
+        saveButton.classList.remove( 'saving' );
+    }
+}
--- src/components/PostBlog.js
+++ src/components/PostBlog.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Button from 'react-bootstrap/Button'
+import { Form } from 'react-bootstrap';
 
 export default function PostBlog() {
   // const [html, setHtml] = React.useState('my <b>HTML</b>');
@@ -8,27 +10,58 @@ export default function PostBlog() {
   // function onChange(e) {
   //   setHtml(e.target.value);
   // }
+  
+  function handleClick(){
+      console.log("Handle Click");
+  }
+  var editorInstance;
+
   return (
-    <div className="Editor">
+    <div className="editor">
         <h2>Using CKEditor 5 build in React</h2>
+        <Form onSubmit={() => {alert("Wow")}}>
         <CKEditor
             editor={ ClassicEditor }
             data="<p>Hello from CKEditor 5!</p>"
             onReady={ editor => {
                 // You can store the "editor" and use when it is needed.
+                editorInstance = editor;
                 console.log( 'Editor is ready to use!', editor );
             } }
             onChange={ ( event, editor ) => {
                 const data = editor.getData();
                 console.log( { event, editor, data } );
+                
             } }
             onBlur={ ( event, editor ) => {
                 console.log( 'Blur.', editor );
+                
             } }
             onFocus={ ( event, editor ) => {
                 console.log( 'Focus.', editor );
             } }
+            onSubmit = { ( event, editor ) => {
+                alert( 'Focus.', editor );
+            } }
+            
         />
+         <Button
+      variant="primary"
+      
+      onClick={() => { 
+        
+        const editorData = editorInstance.getData();      
+        alert("The data is " + editorData) ;
+    
+    }}
+    >
+      Post Blog
+    </Button>
+    </Form>
+        
+       
+       
     </div>
+    
 );
 }
