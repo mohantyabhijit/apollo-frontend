0c1fb05f2ed2a84d08caee67e4b5701ddb7bbd10
Abhijit Mohanty <8387545+mohantyabhijit@users.noreply.github.com>
2021-10-20 23:41:51 +0530

    question/ answer view

--- src/components/BlogsByAuthor.js
+++ src/components/BlogsByAuthor.js
@@ -9,7 +9,6 @@ function BlogsByAuthor(props) {
   const baseUrl = 'http://localhost:9001/v1/blogs/author/';
   const author = props.authorId;
   const authorUrl = baseUrl.concat(author);
-  console.log(authorUrl);
   const [blogIds, setBlogIds] = useState([]);
   useEffect(() => {
     axios.get(authorUrl).then((res) => {
