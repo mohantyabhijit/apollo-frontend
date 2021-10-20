963f6eb21e7ace267c14c199b7521bbc2cd42f4a
Abhijit Mohanty <8387545+mohantyabhijit@users.noreply.github.com>
2021-10-21 01:23:17 +0530

    finished q&a from json file

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -14,8 +14,8 @@ import {
 //   axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 function Questions(props) {
   const baseUrl = 'http://a3e6153cc6ed7427caeae85c27d954c1-1453390660.ap-southeast-1.elb.amazonaws.com:9002/edtech/question/all';
-  console.log(questions);
-  console.log(questions[0].answers);
+  // console.log(questions);
+  // console.log(questions[10].answers);
 //   const blogUrl = baseUrl.concat(props.blogId);
   // const [questions, setBlogTitle] = useState([]);
 
@@ -41,26 +41,20 @@ function Questions(props) {
     marginTop: "5"
   }
 
-  return (
   
-    
+  return (
     <div>
-        {questions.map((c) => (
-          <div style={{margin: 30}}>
-          <Container key={c} fluid >
+        {questions.map((c,index) =>(
+          <div key={index} style={{margin: 30}}>
+          <Container key={index} fluid >
             <h1>
             {c.questionTitle} - {c.author}
             </h1>
-            <Card key={c} style={{margin: 5}}>
-              <Card.Header key={c}> Q : {c.questionText}</Card.Header>
+            <Card key={index} style={{margin: 5}}>
+              <Card.Header key={index}> Q : {c.questionText}</Card.Header>
               <Card.Body>
               <ListGroup variant="flush">
-                {c.anwers != null && c.answers.map((a) => (
-                    <ListGroup.Item key={a} blogId={a}>{a.answerText}</ListGroup.Item>
-                ))}
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                  {c.answers.map(c => <ListGroup.Item>{c.answerText}</ListGroup.Item>)}
               </ListGroup>
               </Card.Body>
             </Card>
@@ -71,6 +65,8 @@ function Questions(props) {
     
   );
 }
+
+
 
 Questions.propTypes = {
 //   blogId: PropTypes.string.isRequired,
