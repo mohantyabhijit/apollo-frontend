90681265f426129f0499d93dab9435b36c4dccf8
Abhijit Mohanty <8387545+mohantyabhijit@users.noreply.github.com>
2021-10-22 18:00:58 +0530

    bug fix for search

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -5,7 +5,8 @@ import { Container, Card } from "react-bootstrap";
 import Blog from "../components/Blog";
 export default function SearchContainer(props) {
     if (props.display){
-        if (props.results != undefined){
+        console.log(props.display[0], props.results);
+        if (props.results != undefined && props.results.length > 0){
             return (
                 <div>
                   <Container fluid>
@@ -15,14 +16,29 @@ export default function SearchContainer(props) {
                       </Card>
                     ))}
                   </Container>
+
                 </div>
               );
                         
-        } else {
+        } else if (props.display[0] === undefined){
+            console.log(props.display[0]);
             return (
-                <div className="App">No results found</div>
+                <div className="App">
+                    
+                    <Container fluid>
+                        
+                            <Card style={{marginTop:20}}>
+                                <Card.Body> No results found </Card.Body>
+                            </Card>
+                        
+                    </Container>
+                    
+                </div>
             )
-    } 
+        } 
+    } else {
+        return null
     }
+
     return null
 }
